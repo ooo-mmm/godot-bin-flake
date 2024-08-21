16a6b99e03c2c19bb34a99418133c9c3739fcf8e
vg <vaibhav7garg@gmail.com>
2024-08-21 22:00:40 +0530

    remove test changes

--- default.nix
+++
@@ -1,6 +0,0 @@
-let
-  flake = builtins.getFlake (toString ./.) { trustTarballsFromGitForge = true; };
-  nixpkgs = import <nixpkgs> { };
-in
-{ inherit flake; }
-// flake.packages.${builtins.currentSystem}
