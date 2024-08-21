42f66daedd9fdf1ca6e432c836095cce478201e8
ooo-mmm <31175327+ooo-mmm@users.noreply.github.com>
2024-08-21 21:30:37 +0530

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,6 @@
 let
-  flake = (import
-    (
-      let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-    )
-    { src = ./.; }
-  ).defaultNix;
+  flake = builtins.getFlake (toString ./.);
+  nixpkgs = import <nixpkgs> { };
 in
-flake.packages.${builtins.currentSystem}
+{ inherit flake; }
+// flake.packages.${builtins.currentSystem}
